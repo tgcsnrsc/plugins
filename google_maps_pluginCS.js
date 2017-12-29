2b51ea842fc398de67ec598a239ea5545bca163b
CodyShive <34662215+CodyShive@users.noreply.github.com>
2017-12-29 15:09:18 -0500

    Richard's Version

--- google_maps_pluginCS.js
+++ google_maps_pluginCS.js
@@ -1,5 +1,3 @@
-
-
 (function () {
 var googleMapWidget = function (settings) {
     var self = this;
@@ -11,19 +9,15 @@ var googleMapWidget = function (settings) {
     this.render = function (element) {
         function initializeMap() {
             var mapOptions = {
-                zoom: 4,
-                center: new google.maps.LatLng(37.235, -115.811111),
+                zoom: 8,
+                //center: new google.maps.LatLng(35.8992, -78.8636),
+                center: new google.maps.LatLng(Number(currentSettings.centerLat), Number(currentSettings.centerLng)),
                 disableDefaultUI: true,
                 draggable: true
             };
             element.style.height="800px";
             map = new google.maps.Map(element, mapOptions);
             
-            // force a resize event to make sure the window gets drawn correctly
-            google.maps.event.addListenerOnce(map, "idle", function(){ 
-                google.maps.event.trigger(map, "resize");
-            });
-
             google.maps.event.addDomListener(element, 'mouseenter', function (e) {
                 e.cancelBubble = true;
                 if (!map.hover) {
@@ -55,43 +49,19 @@ var googleMapWidget = function (settings) {
     this.getHeight = function () {
         return Number(currentSettings.height);
     }
-    window.__gMapsOpenDashboard = function(storeNumber) {
-        var accessVar = {
-            source: {
-                name: "monitor-db-source-"+storeNumber+"-store-stats",
-                settings: {
-                    
-                }
-            }
-        }
-        window.open('http://10.89.153.3:8080/insights/freeboard/index-dev.html#access=eyJkYXNoYm9hcmQiOiI3MDEiLCJyb2xlIjoidXNlciJ9');
-    }
     this.onCalculatedValueChanged = function (settingName, newValue) {
         for(var store in newValue) {
-            let _store = newValue[store];
-            if(_store.alertState==true) {
-                _store.marker = new google.maps.Marker({
-                    position: {lat:Number.parseFloat(_store.latitude),lng:Number.parseFloat(_store.longitude)},
+            store = newValue[store];
+            if(store.alertState==true) {
+                var marker = new google.maps.Marker({
+                    position: {lat:Number.parseFloat(store.latitude),lng:Number.parseFloat(store.longitude)},
                     map: map,
-                    title: "Store Number " + _store.storeNumber
+                    title: "Store Number " + store
                 });
-
-                _store.contentString = '<div id="content">'+
-                '<div id="siteNotice">'+
-                '</div>'+
-                '<h1 id="firstHeading" class="firstHeading">Store Alert</h1>'+
-                '<div id="bodyContent">'+_store.storeNumber+" - "+_store.description+'<br/><br/>'+
-                '<button onclick="__gMapsOpenDashboard('+_store.storeNumber+')">Open Store Dashboard</button>'+
-                '</div>';
-
-                _store.infowindow = new google.maps.InfoWindow({
-                    content: _store.contentString
-                  });
-
-                  _store.marker.addListener('click', function(e) {
-                    // build the data source target for the dashboard .. the 
-                    _store.infowindow.open(map, _store.marker);
-                });
+                //marker.addListener('click', function(e) {
+                    //alert(store);
+                //    window.open('http://10.89.153.3:8080/insights/freeboard/index-dev.html#access=eyJkYXNoYm9hcmQiOiI3MDEiLCJyb2xlIjoidXNlciJ9');
+                //});
             }
         }
     }
@@ -104,12 +74,22 @@ var googleMapWidget = function (settings) {
 
 freeboard.loadWidgetPlugin({
     type_name: "google_map_ext",
-    display_name: "Google Map(RV)",
+    display_name: "Google Map(CS)",
     fill_size: true,
     settings: [
         {
             name: "apikey",
-            display_name: "Google Maps API Key",
+            display_name: "Google API Key",
+            type: "text"
+        },
+        {
+            name: "centerLat",
+            display_name: "Center Latitude",
+            type: "text"
+        },
+        {
+            name: "centerLng",
+            display_name: "Center Longitude",
             type: "text"
         },
         {
@@ -121,11 +101,6 @@ freeboard.loadWidgetPlugin({
             name: "height",
             display_name: "Vertical blocks consumed",
             type: "number"
-        },
-        {
-            name: "dashboard_thing_template",
-            display_name: "Dashboard thing datasource",
-            type: "text"
         }
     ],
     newInstance: function (settings, newInstanceCallback) {
