e4caf721ac17a0c8cd8e67f61f594341c02ddbf0
CodyShive <34662215+CodyShive@users.noreply.github.com>
2018-11-13 07:45:39 -0500

    Update chart_js_plugin.js
    xx

--- chart_js_plugin.js
+++ chart_js_plugin.js
@@ -32,7 +32,8 @@
       var ctx = document.getElementById(currentSettings.id).getContext('2d');
       var myChart = new Chart(ctx, {
         type: 'line',
-	data: {
+
+			data: {
 		labels: ["January", "February", "March", "April", "May", "June", "July"],
 		datasets: [
 		  {
@@ -43,21 +44,14 @@
 			pointStrokeColor: "#fff",
 			pointHighlightFill: "#fff",
 			pointHighlightStroke: "rgba(220,220,220,1)",
-			data: [65, 59, 80, 81, 56, 55, 40]
-		  },
-		  {
-			label: "My Second dataset",
-			fillColor: "rgba(151,187,205,0.2)",
-			strokeColor: "rgba(151,187,205,1)",
-			pointColor: "rgba(151,187,205,1)",
-			pointStrokeColor: "#fff",
-			pointHighlightFill: "#fff",
-			pointHighlightStroke: "rgba(151,187,205,1)",
-			data: [28, 48, 40, 19, 86, 27, 90]
+			data: ["65", "59", "80", "81", "56", "55", "40"]
 		  }
 		]
-	  }; 
-	  }
+		}
+	  });
+	}
+			
+
 
 /*
  data: {
