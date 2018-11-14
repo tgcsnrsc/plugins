f8b27e8c476e6087e1f59c27e6242200fa9e8f48
CodyShive <34662215+CodyShive@users.noreply.github.com>
2018-11-14 12:05:49 -0500

    Update chart_js_plugin.js

--- chart_js_plugin.js
+++ chart_js_plugin.js
@@ -38,7 +38,8 @@
 				{
                 label: currentSettings['dataSetTitle'],
 				fontColor: 'white',
-				data: newValue[currentSettings['plotdata']],
+//				data: newValue[currentSettings['plotdata']],
+				data: [65, 59, 66, 70, 56, 55, 40],
 				borderColor: "turquoise",
 				fill: false
 				}
