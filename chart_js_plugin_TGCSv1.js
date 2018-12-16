e70b37c4489fb12121c5a00a5f54331192949536
CodyShive <34662215+CodyShive@users.noreply.github.com>
2018-12-16 16:23:21 -0500

    Update chart_js_plugin_TGCSv1.js

--- chart_js_plugin_TGCSv1.js
+++ chart_js_plugin_TGCSv1.js
@@ -25,29 +25,38 @@
     var chartHeight = 300 //currentSettings.chartHeight;
     var chartWidth = 600 //currentSettings.chartWidth;
 	//xxx
-	function number_format(number, decimals, dec_point, thousands_sep) {
+	function number_format(formatnumber, number, decimals, dec_point, thousands_sep) {
 	// *     example: number_format(1234.56, 2, ',', ' ');
 	// *     return: '1 234,56'
-		number = (number + '').replace(',', '').replace(' ', '');
-		var n = !isFinite(+number) ? 0 : +number,
-				prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
-				sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
-				dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
-				s = '',
-				toFixedFix = function (n, prec) {
-					var k = Math.pow(10, prec);
-					return '' + Math.round(n * k) / k;
-				};
-		// Fix for IE parseFloat(0.55).toFixed(0) = 0;
-		s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
-		if (s[0].length > 3) {
-			s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+		if (formatnumber == null) {
+			return number;
 		}
-		if ((s[1] || '').length < prec) {
-			s[1] = s[1] || '';
-			s[1] += new Array(prec - s[1].length + 1).join('0');
+		if (formatnumber == false) {
+			return number;
 		}
-		return s.join(dec);
+
+		if (formatnumber == true) {
+			number = (number + '').replace(',', '').replace(' ', '');
+			var n = !isFinite(+number) ? 0 : +number,
+					prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+					sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+					dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+					s = '',
+					toFixedFix = function (n, prec) {
+						var k = Math.pow(10, prec);
+						return '' + Math.round(n * k) / k;
+					};
+			// Fix for IE parseFloat(0.55).toFixed(0) = 0;
+			s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+			if (s[0].length > 3) {
+				s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+			}
+			if ((s[1] || '').length < prec) {
+				s[1] = s[1] || '';
+				s[1] += new Array(prec - s[1].length + 1).join('0');
+			}
+			return s.join(dec);
+		}
 	}
 	//xxx
     //seems to be called once (or after settings change)
@@ -121,10 +130,10 @@
                 beginAtZero: true,
 				//Add formatting to the Y-axis labels.
                 callback: function(value, index, values) {
-                   return '$ ' + number_format(value);
+                   return '$ ' + number_format(newValue[0].yFormatNumber,value);
 				}
 				//userCallback: function(value, index, values) {
-					// Convert the number to a string and splite the string every 3 charaters from the end
+					// Convert the number to a string and split the string every 3 charaters from the end
 					//value = value.toString();
 					//value = value.split(/(?=(?:...)*$)/);
 					// Convert the array to a string and format the output
@@ -167,7 +176,7 @@
 			callbacks: {
 				label: function(tooltipItem, myChart){
 					var datasetLabel = myChart.datasets[tooltipItem.datasetIndex].label || '';
-					return datasetLabel + ': $ ' + number_format(tooltipItem.yLabel, 2);
+					return datasetLabel + ': $ ' + number_format(newValue[0].ToolTipFormatNumber,tooltipItem.yLabel, 2);
 				}
 			}
 		}
