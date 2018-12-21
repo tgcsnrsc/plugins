473b09d5cea8eb36ee5dc56b8b81de7eb3a91e9d
CodyShive <34662215+CodyShive@users.noreply.github.com>
2018-12-21 17:13:31 -0500

    Update chart_js_plugin_TGCS_PIE_DOUGHNUTv2.js

--- chart_js_plugin_TGCS_PIE_DOUGHNUTv2.js
+++ chart_js_plugin_TGCS_PIE_DOUGHNUTv2.js
@@ -41,29 +41,31 @@
       currentSettings = newSettings;
     }
 	 this.pluginService.register({
-	  beforeDraw: function (chartMap[currentSettings.id]) {
-		if (chart.config.options.elements.center) {
+	  beforeDraw: function (chart) {
+		  
+		var myChart = chartMap[currentSettings.id];
+		if (myChart.config.options.elements.center) {
 		  //Get ctx from string
 		  var ctx = chartMap[currentSettings.id];
 
 		  //Get options from the center object in options
-		  var centerConfig = chart.config.options.elements.center;
+		  var centerConfig = myChart.config.options.elements.center;
 		  var fontStyle = centerConfig.fontStyle || 'Arial';
 		  var txt = centerConfig.text;
 		  var color = centerConfig.color || '#000';
 		  var sidePadding = centerConfig.sidePadding || 20;
-		  var sidePaddingCalculated = (sidePadding/100) * (chart.innerRadius * 2)
+		  var sidePaddingCalculated = (sidePadding/100) * (myChart.innerRadius * 2)
 		  //Start with a base font of 30px
 		  ctx.font = "30px " + fontStyle;
 
 		  //Get the width of the string and also the width of the element minus 10 to give it 5px side padding
 		  var stringWidth = ctx.measureText(txt).width;
-		  var elementWidth = (chart.innerRadius * 2) - sidePaddingCalculated;
+		  var elementWidth = (myChart.innerRadius * 2) - sidePaddingCalculated;
 
 		  // Find out how much the font can grow in width.
 		  var widthRatio = elementWidth / stringWidth;
 		  var newFontSize = Math.floor(30 * widthRatio);
-		  var elementHeight = (chart.innerRadius * 2);
+		  var elementHeight = (myChart.innerRadius * 2);
 
 		  // Pick a new font size so it will not be larger than the height of label.
 		  var fontSizeToUse = Math.min(newFontSize, elementHeight);
@@ -71,8 +73,8 @@
 		  //Set font settings to draw it correctly.
 		  ctx.textAlign = 'center';
 		  ctx.textBaseline = 'middle';
-		  var centerX = ((chart.chartArea.left + chart.chartArea.right) / 2);
-		  var centerY = ((chart.chartArea.top + chart.chartArea.bottom) / 2);
+		  var centerX = ((myChart.chartArea.left + myChart.chartArea.right) / 2);
+		  var centerY = ((myChart.chartArea.top + myChart.chartArea.bottom) / 2);
 		  ctx.font = fontSizeToUse+"px " + fontStyle;
 		  ctx.fillStyle = color;
 
