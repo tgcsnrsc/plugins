fdece62951a8974302988572b2db7e09d0ca6e91
CodyShive <34662215+CodyShive@users.noreply.github.com>
2018-12-22 13:21:55 -0500

    Update chart_js_plugin_TGCS_PIE_DOUGHNUTv2.js

--- chart_js_plugin_TGCS_PIE_DOUGHNUTv2.js
+++ chart_js_plugin_TGCS_PIE_DOUGHNUTv2.js
@@ -15,6 +15,35 @@
  * * 11-19-2018		Shive		New for Pie or Doughnut w/text inside hole
  * * ============================================================================
  */
+	Chart.Chart.pluginService.register({
+		beforeDraw: function(chart) {
+			if (chart.config.centerText.display !== null &&
+				typeof chart.config.centerText.display !== 'undefined' &&
+				chart.config.centerText.display) {
+				drawTotals(chart);
+			}
+		},
+	});
+	 
+	function drawTotals(chart) {
+	 
+		var width = chart.chart.width,
+		height = chart.chart.height,
+		ctx = chart.chart.ctx;
+	 
+		ctx.restore();
+		var fontSize = (height / 114).toFixed(2);
+		ctx.font = fontSize + "em sans-serif";
+		ctx.textBaseline = "middle";
+	 
+		var text = chart.config.centerText.text,
+		textX = Math.round((width - ctx.measureText(text).width) / 2),
+		textY = height / 2;
+	 
+		ctx.fillText(text, textX, textY);
+		ctx.save();}
+		
+		
   var chartMap = {};
   var chart_js_plugin_TGCS_PIE_DOUGHNUTv2 = function (settings) {
     var self = this;    
